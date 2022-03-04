0996f0508d6bb23f8b58ea7c785617c3c04e90cb
Calaf-Ebra <80424635+Calaf-Ebra@users.noreply.github.com>
2022-03-04 23:10:41 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import streamlit as st
 
 
 
-st.set_page_config(page_title= "VV")
-st.title("Litttt")
-st.subheader('GGGG')
+st.set_page_config(page_title= "Tech Product ")
+st.title("Tech Product")
+st.subheader('Automatation of Accoutancy System with Python')
 
