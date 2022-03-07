d5199e93f12d54f283299eaecfd145e60404291b
Calaf-Ebra <80424635+Calaf-Ebra@users.noreply.github.com>
2022-03-07 21:56:49 +0300

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,8 @@ import streamlit as st
 option = st.selectbox(
      'How would you like to be contacted?',
      ('Email', 'Home phone', 'Mobile phone'))
+for x in option:
+     if option=='Email'
+       print("hh")
 
 st.write('You selected:', option)
