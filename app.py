3e3eea5fa6b20b9c46e0b1bc032c5e83e8eacb30
Calaf-Ebra <80424635+Calaf-Ebra@users.noreply.github.com>
2022-03-08 23:49:04 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,9 +1,13 @@
 import streamlit as st
 
-a='jj'
+a='cass'
 
 option = st.selectbox(
      'How would you like to be contacted?',
      (a, 'Home phone', 'Mobile phone'))
+
+if option==a:
+     st.header("We are done")
+     
 
 st.write('You selected:', option)
