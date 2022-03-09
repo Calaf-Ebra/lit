05a830d7caba6942f149093ae25059ecd51a9855
Calaf-Ebra <80424635+Calaf-Ebra@users.noreply.github.com>
2022-03-09 22:23:02 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.header("LWP")
 st.subheader("Automation Solutions")
 
 
-sheet_id = “/1IXe_43DiORo36E9D7OgUiCQQD9JeYOtIl0HimoStOyA/edit#gid=0”
+sheet_id = "/1IXe_43DiORo36E9D7OgUiCQQD9JeYOtIl0HimoStOyA/edit#gid=0”
 sheet_name = “Leave with Python”
 url = f”https://docs.google.com/spreadsheets/d/1IXe_43DiORo36E9D7OgUiCQQD9JeYOtIl0HimoStOyA/edit#gid=0"
   pd.read_csv(url)
