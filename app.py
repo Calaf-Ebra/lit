536d4cd8a6f6dfbd9ca4fdb7349582189f105b04
Calaf-Ebra <80424635+Calaf-Ebra@users.noreply.github.com>
2022-03-09 22:17:57 +0300

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ st.header("LWP")
 st.subheader("Automation Solutions")
 
 
-sheet_url = “https://docs.google.com/spreadsheets/d/1IXe_43DiORo36E9D7OgUiCQQD9JeYOtIl0HimoStOyA/edit#gid=0"
+sheet_url = "https://docs.google.com/spreadsheets/d/1IXe_43DiORo36E9D7OgUiCQQD9JeYOtIl0HimoStOyA/edit#gid=0"
 url_1 = sheet_url.replace(‘/edit#gid=’, ‘/export?format=csv&gid=’)
 pd.read_csv(url_1)
                           
