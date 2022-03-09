c29868992c82628031ee77618f130cfc873fb01c
Calaf-Ebra <80424635+Calaf-Ebra@users.noreply.github.com>
2022-03-09 22:16:55 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,15 @@
 import streamlit as st
+import pandas as pd
+
 st.header("LWP")
 st.subheader("Automation Solutions")
 
+
+sheet_url = “https://docs.google.com/spreadsheets/d/1IXe_43DiORo36E9D7OgUiCQQD9JeYOtIl0HimoStOyA/edit#gid=0"
+url_1 = sheet_url.replace(‘/edit#gid=’, ‘/export?format=csv&gid=’)
+pd.read_csv(url_1)
+                          
+                          
 a='cass'
 
 number = st.number_input('Number of Items')
