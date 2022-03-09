0fb3043d23716f9ec4f6870989a530b0b924ec64
Calaf-Ebra <80424635+Calaf-Ebra@users.noreply.github.com>
2022-03-09 20:55:16 +0300

    Update app.py

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ number = st.number_input('Number of Items')
 options = st.selectbox(
      'Item Name?',
      ('Farida',a, 'Holland'))
-
-st.write('You selected:', options)
 if options==a:
      st.write(number*2)
      
