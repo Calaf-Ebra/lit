d5dc004ee5a72cff855dd8bdc97f0652ef997c73
Calaf-Ebra <80424635+Calaf-Ebra@users.noreply.github.com>
2022-03-09 20:46:17 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,8 +7,8 @@ a='cass'
 number = st.number_input('Number of Items')
 options = st.multiselect(
      'What are your favorite colors',
-     ['Green', 'Yellow', 'Red', 'Blue'],
-     ['Yellow', 'Red'])
+     ['Farida',a, 'BG', 'Janov'],
+     ['Extra', 'KF'])
 
 if options==a:
      st.write(number*2)
