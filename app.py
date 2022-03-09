497fbb87bda553723461b4a1fd5c3ffd5aaa9c60
Calaf-Ebra <80424635+Calaf-Ebra@users.noreply.github.com>
2022-03-09 20:27:22 +0300

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ number = st.number_input('Insert a number')
 
 option = st.selectbox(
      'How would you like to be contacted?',
-     ("hhh',a, 'Home phone', 'Mobile phone'))
+     ('hhh',a, 'Home phone', 'Mobile phone'))
 
 if option==a:
      st.write(number*2)
